090561b394c346e1599d15f264c1004d37afc88a
Andrzej Iwanski <iwanski@fev.com>
2020-02-24 15:01:19 +0100

    Add style, add router.

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
+import {Route, NavLink, HashRouter} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
 
-class Main extends React.Component {
+
+class Main extends Component {
     render() {
         return (
-            <div>
-                <h1>Prosta aplikacja jednostronowa</h1>
-                <ul className="header">
-                    <li><a href="/">Strona glowna</a></li>
-                    <li><a href="/stuff">O nas</a></li>
-                    <li><a href="/contact">Kontakt</a></li>
-                </ul>
-            </div>
+            <HashRouter>
+                <div>
+                    <h1>Prosta aplikacja jednostronowa</h1>
+                    <ul className="header">
+                        <li><NavLink exact to="/">Strona glowna</NavLink></li>
+                        <li><NavLink to="/stuff">O nas</NavLink></li>
+                        <li><NavLink to="/contact">Kontakt</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/stuff" component={Stuff}/>
+                        <Route path="/contact" component={Contact}/>
+                    </div>
+                </div>
+            </HashRouter>
         );
     }
 }
