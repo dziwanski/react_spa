0db36e96cd38f4705241303c0c9a4ca71bd11cf3
Andrzej Iwanski <iwanski@fev.com>
2020-02-24 14:20:08 +0100

    Add new components.

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Masz pytanie?</h2>
+                <p>Najlepiej wyslij wiadomosc przez nasze <a href="http://forum.kirupa.com">forum</a></p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Stuff extends React.Component{
+    render() {
+        return (
+            <div>
+                <h2>Opis</h2>
+                <p>safdfsadfaffd</p>
+                <ol>
+                    <li>aaaaaaaaaaa</li>
+                    <li>bbbbbbbbbbb</li>
+                    <li>ccccccccccc</li>
+                    <li>ddddddddddd</li>
+                    <li>eeeeeeeeeee</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
